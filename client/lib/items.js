d9ba6406b0caede75eba6c548b5e38922cde88ac
Marius Küng <apps@mariuskueng.ch>
2015-03-25 22:06:32 +0100

    Add new items to current list

--- client/lib/items.js
+++ client/lib/items.js
@@ -2,15 +2,28 @@ Meteor.subscribe('Items');
 
 Template.itemsView.helpers({
   items: function() {
-    return Items.find({createdBy: Meteor.userId}, {sort:{createdAt: -1}});
+    var controller = Iron.controller();
+    var params = controller.getParams();
+
+    return Items.find({
+      createdBy: Meteor.userId(),
+      list: params._id
+    }, {
+      sort:{
+        createdAt: -1
+      }
+    });
   }
 });
 
 Template.itemsView.events({
   "submit .new-item": function (event) {
     // This function is called when the new item form is submitted
+    var controller = Iron.controller();
+    var params = controller.getParams();
+
     var text = event.target.text.value;
-    Meteor.call("addItem", text, this.params._id);
+    Meteor.call("addItem", text, params._id);
 
     // Clear form
     event.target.text.value = "";
