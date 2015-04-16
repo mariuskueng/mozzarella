f554f57d1388a41b8b06ec5f3d23a146a40e82ab
Marius Küng <maku@mailbox.org>
2015-04-16 16:38:38 +0200

    Prevent adding a list with no title

--- client/lib/lists.js
+++ client/lib/lists.js
@@ -32,6 +32,7 @@ Template.addListView.events({
   'submit .add-list': function (event, template) {
     // This function is called when the new item form is submitted
     var text = event.target.text.value;
+    if (text === '') return false;
     Meteor.call("addList", text);
 
     // Clear form
