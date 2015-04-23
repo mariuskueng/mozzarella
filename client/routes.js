7d2a294c3b5e60ed054a2a917ac7cf8e01bd960e
Marius Küng <maku@mailbox.org>
2015-04-23 08:38:24 +0200

    Router refactoring

--- client/routes.js
+++ client/routes.js
@@ -15,67 +15,72 @@ Router.configure({
 // name 'about' that
 // matches '/about' and automatically renders
 // template 'about'
-Router.map( function () {
-  this.route('about');
+Router.route('about', function() {
+  this.render('about');
+});
 
-  // simple route with
-  // name 'appView' that
-  // matches '/' and automatically renders
-  // template 'appView'
-  this.route('listsView', {
-    path: '/lists/:_id',
-    template: 'appView',
-    onBeforeAction: function () {
-      if (!Meteor.userId()) {
-        // if the user is not logged in, render the Login template
-        this.render('homeView');
-      } else {
-        // set the current list before rendering
-        Meteor.call('setCurrentList', this.params._id);
-        // otherwise don't hold up the rest of hooks or our route/action function
-        // from running
-        this.next();
-      }
-    },
-    action: function () {
-      this.render();
+// simple route with
+// name 'appView' that
+// matches '/' and automatically renders
+// template 'appView'
+Router.route('listsView', {
+  path: '/lists/:_id',
+  template: 'appView',
+  onBeforeAction: function () {
+    if (!Meteor.userId()) {
+      // if the user is not logged in, render the Login template
+      this.render('homeView');
+    } else {
+
+      // set the current list before rendering
+      Meteor.call('setCurrentList', this.params._id);
+      // otherwise don't hold up the rest of hooks or our route/action function
+      // from running
+      this.next();
     }
-  });
+  },
+  action: function () {
+    this.render();
+  }
+});
 
-  this.route('allView', {
-    path: '/lists/all',
-    template: 'appView'
-  });
+// redirect lists root to all
+Router.route('/lists/', function() {
+  this.redirect('/lists/all');
+});
 
-    this.route('overDueView', {
-    path: '/lists/over-due',
-    template: 'appView'
-  });
+Router.route('allView', {
+  path: '/lists/all',
+  template: 'appView'
+});
 
-  this.route('appView', {
-    path: '/',
-    onBeforeAction: function () {
-      if (!Meteor.userId()) {
-        // if the user is not logged in, render the Login template
-        this.render('homeView');
-      } else {
-        // otherwise don't hold up the rest of hooks or our route/action function
-        // from running
-        this.next();
-      }
-    },
-    action: function() {
-      // passes the last opened user's list (MAGIC)
-      var user = Meteor.user();
-      if (user.profile && user.profile.lastOpenedList) {
-        var lastList = Lists.findOne(Meteor.user().profile.lastOpenedList);
-        if (lastList && lastList._id) {
-          Router.go('listsView', lastList);
-        }
-      } else {
-        Router.go('allView', 'all');
-      }
+Router.route('overDueView', {
+  path: '/lists/over-due',
+  template: 'appView'
+});
+
+Router.route('appView', {
+  path: '/',
+  onBeforeAction: function () {
+    if (!Meteor.userId()) {
+      // if the user is not logged in, render the Login template
+      this.render('homeView');
+    } else {
+      // otherwise don't hold up the rest of hooks or our route/action function
+      // from running
+      this.next();
     }
-  });
-
+  },
+  action: function() {
+    // passes the last opened user's list (MAGIC)
+    var user = Meteor.user();
+    if (user.profile && user.profile.lastOpenedList) {
+      var lastList = Lists.findOne(Meteor.user().profile.lastOpenedList);
+      if (lastList && lastList._id) {
+        Router.go('listsView', lastList);
+      }
+    } else {
+      Router.go('allView', 'all');
+    }
+  }
 });
