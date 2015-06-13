0443bd7963ae47e5290f8465cbdb43ab586a45d8
Marius Küng <maku@mailbox.org>
2015-06-13 10:48:45 +0200

    Added about modal

--- client/views/common/_loginButtonsAdditionalLoggedInDropdownActions.js
+++ client/views/common/_loginButtonsAdditionalLoggedInDropdownActions.js
@@ -1,8 +1,3 @@
 Template._loginButtonsLoggedInDropdown.events({
-    'click #login-buttons-about': function(event) {
-        Router.go('about');
-    },
-    'click #login-buttons-settings': function(event) {
-        $('#settings-modal-dialog').modal('show');
-    },
+
 });
