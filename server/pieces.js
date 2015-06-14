ed1849b57b21e30e86e7361c29aa9290611163d9
Marius Küng <maku@mailbox.org>
2015-06-14 19:38:32 +0200

    Limit publishing pieces

--- server/pieces.js
+++ server/pieces.js
@@ -1,11 +1,23 @@
 Meteor.publish("Pieces", function(){
-  // var items = Items.find()
-  // .map(function(item){ return item._id; });
-  //
-  // var pieces = Pieces.find({
-  //   list: {$in: lists}
-  // });
-  //
-  // return pieces;
-  return Pieces.find({});
+  var lists = Lists.find(
+    {
+      $or: [
+        { users: this.userId },
+        { createdBy: this.userId }
+      ]
+    },
+    {
+      _id: 1
+    }
+  ).map(function(list){ return list._id; });
+
+  var items = Items.find({
+    list: {$in: lists}
+  }).map(function(item) { return item._id; });
+
+  var pieces = Pieces.find({
+    item: {$in: items}
+  });
+
+  return pieces;
 });
